21ddd552c0a29cabc24cf15e80c8f73b774ef6b2
CliffLinusthomi <linusthomi2@gmail.com>
2025-05-12 10:16:13 +0300

    first commit

--- lib/person.py
+++ lib/person.py
@@ -3,5 +3,18 @@
 class Person:
     # Class body goes here
 
-    #Instance method definition
-    pass
+    def talk(self):
+        print("Hello World!")
+
+    def walk(self):
+        print("The person is walking.")    
+cliff = Person()
+cliff.talk()
+cliff.walk()        
+
+    # #Instance method definition
+    # def __init__(self, name, age):
+    #     self.name = name
+    #     self.age = age
+    #     self.friends = []
+    #     self.hobbies = []
